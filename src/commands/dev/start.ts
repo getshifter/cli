8a58959996d7cddd0699267e5df19ce7a3404569
Daniel Olson <emaildano@gmail.com>
2021-11-03 21:14:11 -0400

    Added new files

---
+++ src/commands/dev/start.ts
@@ -0,0 +1,31 @@
+import { Command, flags } from "@oclif/command";
+
+const { exec } = require("child_process");
+
+export default class DevStart extends Command {
+  static description = "describe the command here";
+
+  static flags = {
+    help: flags.help({ char: "h" }),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({ char: "n", description: "name to print" }),
+    // flag with no value (-f, --force)
+    force: flags.boolean({ char: "f" }),
+  };
+
+  static args = [{ name: "file" }];
+
+  async run() {
+    exec("docker-compose up", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
+}
